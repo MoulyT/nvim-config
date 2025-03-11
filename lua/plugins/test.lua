886ec1710f3f8c655f772cb9dfec312db9f7bc62
Moulay Taib Taha Sarkadi <mouly.taha@gmail.com>
2025-03-11 10:46:55 +0100

    feat(neotest): configure test for vitest

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,21 +1,11 @@
 return {
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "haydenmeade/neotest-jest",
+  "nvim-neotest/neotest",
+  dependencies = {
+    "marilari88/neotest-vitest",
+  },
+  opts = {
+    adapters = {
+      ["neotest-vitest"] = {},
     },
-    opt = function(_, opts)
-      table.insert(
-        opts.adapters,
-        require("neotest-jest")({
-          jestCommand = "yarn test --",
-          jestConfigFile = "custom.jest.config.ts",
-          env = { CI = true },
-          cwd = function()
-            return vim.fn.getcwd()
-          end,
-        })
-      )
-    end,
   },
 }
